6accbd0f4a12567d4353dee48e7114c9d7171a09
Matteo Cacciola <matteo.cacciola@gmail.com>
2024-06-25 22:14:15 +0200

    mongodb

--- fastapi_healthz/service/mongodb.py
+++ fastapi_healthz/service/mongodb.py
@@ -6,10 +6,18 @@ from ..models import HealthCheckStatusEnum
 
 
 class HealthCheckMongoDb(HealthCheckAbstract):
-    def __init__(self, uri: str, service: str | None = None, tags: list[str] | None = None, **kwargs: Any):
+    def __init__(
+        self,
+        host: str | None,
+        port: str | None,
+        service: str | None = None,
+        tags: list[str] | None = None,
+        **kwargs: Any
+    ):
         super().__init__(service=service, tags=tags)
 
-        self.__uri = uri
+        self.__host = host
+        self.__port = port
         self.__kwargs = kwargs
 
     @property
@@ -18,13 +26,13 @@ class HealthCheckMongoDb(HealthCheckAbstract):
 
     @property
     def connection_uri(self) -> str | None:
-        return self.__uri
+        return f"mongodb://{self.__host}:{self.__port}" if self.__host and self.__port else None
 
     def check_health(self) -> HealthCheckStatusEnum:
         res: HealthCheckStatusEnum = HealthCheckStatusEnum.UNHEALTHY
 
         try:
-            client = MongoClient(**self.__kwargs)
+            client = MongoClient(host=self.__host, port=self.__port, **self.__kwargs)
             client.server_info()
             res = HealthCheckStatusEnum.HEALTHY
         except Exception:
