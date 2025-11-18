89bc98080c516c81f56d47fe2aa672088a1d92e1
Matteo Cacciola <matteo.cacciola@gmail.com>
2025-11-18 17:03:05 +0100

    fix

--- fastapi_healthz/service/__init__.py
+++ fastapi_healthz/service/__init__.py
@@ -3,5 +3,5 @@ from .mongodb_check import HealthCheckMongoDb
 from .rabbitmq_check import HealthCheckRabbitMQ
 from .database_check import HealthCheckDatabase
 from .redis_check import HealthCheckRedis
-from .s3 import HealthCheckS3
+from .s3_check import HealthCheckS3
 from .uri_check import HealthCheckUri
